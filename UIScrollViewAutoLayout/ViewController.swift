c42ef255cb7f87a09ec93db8acd37d306c58aadf
Alugaddala, Ashish Kumar [GCB-OT] <aa80452@imcnam.ssmb.com>
2018-03-08 14:45:46 -0500

    Initial Commit

---
+++ UIScrollViewAutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIScrollViewAutoLayout
+//
+//  Created by Alugaddala, Ashishkumar [GCB-OT] on 3/8/18.
+//  Copyright © 2018 Alugaddala, Ashish Kumar [GCB-OT]. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
